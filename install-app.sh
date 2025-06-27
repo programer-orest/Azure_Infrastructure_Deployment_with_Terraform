f3ca536131239f04882dbf9ce1314758ae5171b0
Орест Якимів <58883508+programer-orest@users.noreply.github.com>
2025-06-27 15:57:46 +0300

    Update install-app.sh

--- install-app.sh
+++ install-app.sh
@@ -3,7 +3,7 @@
 # Script to silently install and start the todo web app on the virtual machine. 
 # Note that all commands bellow are without sudo - that's because extention mechanism 
 # runs scripts under root user. 
-
+git clone https://github.com/programer-orest/devops_todolist_terraform_task.git
 # install system updates and isntall python3-pip package using apt. '-yq' flags are 
 # used to suppress any interactive prompts - we won't be able to confirm operation 
 # when running the script as VM extention.  
